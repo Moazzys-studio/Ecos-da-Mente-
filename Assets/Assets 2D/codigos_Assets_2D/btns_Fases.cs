42e2a682ffe3f2a99ee967330c1f71768fedcb52
Moazzys <marcellematos46@gmail.com>
2025-11-17 01:08:01 -0300

    a acessibilidade voltou gloria

---
+++ Assets/Assets 2D/codigos_Assets_2D/btns_Fases.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btns_Fases : MonoBehaviour
+{
+    public void Fase_1()
+    {
+      SceneManager.LoadScene("Infinite runner");
+    }
+}
